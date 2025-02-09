96faf29fc4756bed1b13af06e034aea19599d9b5
chinedunewbirth <chineduchukwuemekamazi@gmail.com>
2025-02-09 18:48:05 +0000

    updated testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,11 +2,15 @@ import unittest
 from app.main import app
 
 class TestApp(unittest.TestCase):
-     def test_hello_world(self):
+     def setUp(self):
         self.app = app.test_client()
+        self.app.testing = True
+
+     def test_hello_world(self):
         response = self.app.get('/')
+        html_content = response.data.decode('utf-8')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hello, World!')
+        self.assertIn("<h1>Hello, World!", html_content)
 
 
 if __name__ == '__main__':
